c8f9389d0080053f4b66d1ba1635760810a3335e
shangzhouye <shangzhouye@gmail.com>
2020-10-10 22:33:40 -0500

    Added sensor bringup

---
+++ setup_ros.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# run this script on jackal robot
+export JACKAL_PS3=1
+export ROS_MASTER_URI=http://jackal-desktop:11311
+export ROS_HOSTNAME=jackal-desktop
+source /home/jakal/jackal_ws/devel/setup.bash
