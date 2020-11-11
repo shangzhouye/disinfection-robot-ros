6dc738d398829ba3ffc97f57145ce4d503339cc5
shangzhouye <shangzhouye@gmail.com>
2020-11-11 15:49:12 -0600

    Setup camera lidar transforms

--- rgbd_object_detection/include/rgbd_object_detection/camera_utils.hpp
+++ rgbd_object_detection/include/rgbd_object_detection/camera_utils.hpp
@@ -2,6 +2,19 @@
 #define CAMERA_UTILS_INCLUDE_GUARD_HPP
 /// \file
 /// \brief Camera model and camera lidar fusion
+///
+
+/*! Definition of extrinsics
+ *  map -> ... -> base_link -> velodyne -> camera_link -> camera_color_optical_frame
+ *      (obtained from slam)         (extrinsic)    (given by camera)
+ *   
+ *                    z  x                          / z         
+ *                    | /                          /_ _ x                  
+ *               y _ _|/                          |                       
+ *                                                | y                       
+ *               camera_link              camera_color_optical_frame                 
+ *  
+*/
 
 // ROS
 #include "ros/ros.h"
@@ -13,6 +26,17 @@
 namespace disinfection_robot
 {
 
+Eigen::Affine3d create_rotation_matrix(double ax, double ay, double az)
+{
+    Eigen::Affine3d rx =
+        Eigen::Affine3d(Eigen::AngleAxisd(ax, Eigen::Vector3d(1, 0, 0)));
+    Eigen::Affine3d ry =
+        Eigen::Affine3d(Eigen::AngleAxisd(ay, Eigen::Vector3d(0, 1, 0)));
+    Eigen::Affine3d rz =
+        Eigen::Affine3d(Eigen::AngleAxisd(az, Eigen::Vector3d(0, 0, 1)));
+    return rz * ry * rx;
+}
+
 class Camera
 {
 public:
@@ -21,10 +45,7 @@ public:
     float color_fx_;
     float color_fy_; // Camera intrinsics
 
-    Eigen::Matrix3d Rcl_; // extrinsic, from camera to lidar frame transformation
-    Eigen::Matrix<double, 3, 1> tcl_;
-
-    Eigen::Matrix<double, 4, 4> Tlm_; // from lidar to map
+    Eigen::Matrix4d Tco2l_; // extrinsic, from camera optical to lidar frame transformation
 
     Camera();
 
@@ -37,14 +58,28 @@ public:
         nh.getParam("/color_fx", color_fx_);
         nh.getParam("/color_fy", color_fy_);
 
-        std::vector<double> Rcl_temp;
-        std::vector<double> tcl_temp;
-        nh.getParam("/Rcl", Rcl_temp);
-        nh.getParam("/tcl", tcl_temp);
-        Rcl_ << Rcl_temp[0], Rcl_temp[1], Rcl_temp[2],
-            Rcl_temp[3], Rcl_temp[4], Rcl_temp[5],
-            Rcl_temp[6], Rcl_temp[7], Rcl_temp[8];
-        tcl_ << tcl_temp[0], tcl_temp[1], tcl_temp[2];
+        std::vector<double> rpy_lc;
+        std::vector<double> t_lc;
+        nh.getParam("/rpy_lc", rpy_lc);
+        nh.getParam("/t_lc", t_lc);
+        Eigen::Affine3d Rot_lc = create_rotation_matrix(rpy_lc[0], rpy_lc[1], rpy_lc[2]);
+        Eigen::Affine3d Trans_lc(Eigen::Translation3d(Eigen::Vector3d(t_lc[0], t_lc[1], t_lc[2])));
+        // transformation from camera link to lidar
+        Eigen::Affine3d T_cl = (Trans_lc * Rot_lc).inverse();
+
+        std::vector<double> rpy_c2co;
+        std::vector<double> t_c2co;
+        nh.getParam("/rpy_c2co", rpy_c2co);
+        nh.getParam("/t_c2co", t_c2co);
+        Eigen::Affine3d Rot_c2co = create_rotation_matrix(rpy_c2co[0], rpy_c2co[1], rpy_c2co[2]);
+        Eigen::Affine3d Trans_c2co(Eigen::Translation3d(Eigen::Vector3d(t_c2co[0], t_c2co[1], t_c2co[2])));
+        // transformation from camera optical frame to camera link
+        Eigen::Affine3d T_co2c = (Trans_c2co * Rot_c2co).inverse();
+
+        Tco2l_ = (T_co2c * T_cl).matrix();
+
+        // validated by comparing to ros tf
+        // std::cout << Tco2l_ << std::endl;
     }
 
     /*! \brief return 3 by 3 intrinsic matrix
@@ -60,7 +95,7 @@ public:
     */
     Eigen::Matrix<double, 3, 1> lidar2camera(const Eigen::Matrix<double, 3, 1> &p_l)
     {
-        return Rcl_ * p_l + tcl_;
+        return (Tco2l_ * p_l.colwise().homogeneous()).colwise().hnormalized();
     }
 
     /*! \brief transform a point from camera coordinate to sensor coordinate
@@ -80,10 +115,11 @@ public:
     }
 
     /*! \brief transform a point from lidar coordinate to map coordinate
+    *           Given map to lidar transform and 3D point
     */
-    Eigen::Matrix<double, 3, 1> lidar2map(const Eigen::Matrix<double, 3, 1> &p_l)
+    Eigen::Matrix<double, 3, 1> lidar2map(const Eigen::Matrix<double, 3, 1> &p_l, const Eigen::Matrix<double, 4, 4> &Tml)
     {
-        return (Tlm_ * p_l.colwise().homogeneous()).colwise().hnormalized();
+        return (Tml * p_l.colwise().homogeneous()).colwise().hnormalized();
     }
 
     /*! \brief given a pixel position and a depth, transfer it into a 3D point in camere frame (all in color camera frame)
