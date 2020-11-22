609a941ed960839e7e4b4d4bacd0ad6844f0b221
shangzhouye <shangzhouye@gmail.com>
2020-11-21 22:51:44 -0600

    Setup on Jackal

--- setup_beast.bash
+++ setup_beast.bash
@@ -1,3 +1,4 @@
 #!/bin/bash 
 export ROS_MASTER_URI=http://jackal-desktop:11311 
 export ROS_HOSTNAME=beast 
+source ../../devel/setup.bash
--- setup_laptop.bash
+++ setup_laptop.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
 export ROS_MASTER_URI=http://jackal-desktop:11311
 export ROS_HOSTNAME=lucky27 
+source ../../devel/setup.bash
